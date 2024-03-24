c5dca87201260fd6a3235e09b42135fb7538cecd
Felipe Franco <felipejf739@hotmail.com>
2024-03-24 17:17:14 -0300

    save

---
+++ Back-end/SCHapi/SCHapi/src/main/java/com/example/SCHapi/model/entity/CamaTipoQuarto.java
@@ -0,0 +1,25 @@
+package com.example.SCHapi.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CamaTipoQuarto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantidade;
+
+
+    @ManyToOne
+    private TipoCama tipoCama;
+}
