57ee7ad5957b1fdddb002452b451259e9b2da06f
Felipe Franco <felipejf739@hotmail.com>
2024-03-24 17:28:38 -0300

    save

---
+++ Back-end/SCHapi/SCHapi/src/main/java/com/example/SCHapi/api/dto/StatusHospedagemDTO.java
@@ -0,0 +1,14 @@
+package com.example.SCHapi.api.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusHospedagemDTO {
+
+    private Long id;
+
+    private String titulo;
+}
